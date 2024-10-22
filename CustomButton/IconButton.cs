62f63ba48fb7ecc0085a2ee6e7703a3ae67f13c8
mtmattei <matthew@platform.uno>
2024-10-22 14:27:12 -0400

    added IconSize property

--- CustomButton/IconButton.cs
+++ CustomButton/IconButton.cs
@@ -14,12 +14,22 @@ public class IconButton : Button
 {
     public static readonly DependencyProperty IconSourceProperty =
         DependencyProperty.Register(nameof(IconSource), typeof(IconSource), typeof(IconButton),
-            new PropertyMetadata(null));  // PropertyChanged callback no longer needed
+            new PropertyMetadata(null));
+
+    public static readonly DependencyProperty IconSizeProperty =
+        DependencyProperty.Register(nameof(IconSize), typeof(double), typeof(IconButton),
+            new PropertyMetadata(20.0));
 
     public IconSource IconSource
     {
         get => (IconSource)GetValue(IconSourceProperty);
         set => SetValue(IconSourceProperty, value);
+    }
+
+    public double IconSize
+    {
+        get => (double)GetValue(IconSizeProperty);
+        set => SetValue(IconSizeProperty, value);
     }
 
     public IconButton()
